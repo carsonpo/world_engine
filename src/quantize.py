b90fc0172ec99231b4ce09dcef7129d064d5a4f1
Andrew Lapp <andrew@github.rew.la>
2026-01-05 15:33:45 -0500

    pin to stable torch

--- src/quantize.py
+++ src/quantize.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 
 
-QUANTS = [None, "w8a8"]
+QUANTS = [None]  # TODO: enable specific quant based on model config, which should specify compatible quants
 
 
 try:
